147e2d21af926e9535358324ac0c0134fbd263c3
Sarvesh Roshan <lalr.466@gamil.com>
2026-02-11 21:01:05 +0530

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Sarvesh Roshan on 11/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
