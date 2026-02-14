5db364c835c8a6acac1507fa2e07a87b17d419f2
Sarvesh Roshan <lalr.466@gamil.com>
2026-02-14 19:33:50 +0530

    Learnings updated

---
+++ SwiftUILearning/HapticsTutorial.swift
@@ -0,0 +1,18 @@
+//
+//  HapticsTutorial.swift
+//  SwiftUILearning
+//
+//  Created by Sarvesh Roshan on 14/02/26.
+//
+
+import SwiftUI
+
+struct HapticsTutorial: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HapticsTutorial()
+}
---
+++ SwiftUILearning/MultipleSheetsTutorial.swift
@@ -0,0 +1,18 @@
+//
+//  MultipleSheetsTutorial.swift
+//  SwiftUILearning
+//
+//  Created by Sarvesh Roshan on 14/02/26.
+//
+
+import SwiftUI
+
+struct MultipleSheetsTutorial: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultipleSheetsTutorial()
+}
