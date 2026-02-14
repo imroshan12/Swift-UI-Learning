84b0f4b5349640042b049d51d0f346cd856c9a72
Sarvesh Roshan <lalr.466@gamil.com>
2026-02-14 21:26:21 +0530

    Hashable tutorial added

--- SwiftUILearning/HashableTutorial.swift
+++ SwiftUILearning/HashableTutorial.swift
@@ -7,9 +7,33 @@
 
 import SwiftUI
 
+struct MyCustomModel: Hashable {
+    let title: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+    }
+}
+
 struct HashableTutorial: View {
+    
+    let data: [MyCustomModel] = [
+        MyCustomModel(title: "One"),
+        MyCustomModel(title: "Two"),
+        MyCustomModel(title: "Three"),
+        MyCustomModel(title: "Four")
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                ForEach(data, id: \.self) { item in
+                    Text(item.title)
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                }
+            }
+        }
     }
 }
 
